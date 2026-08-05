4b5762133fd2b5d83d5afa10d3e8ffc35ea43596
rihannabet1212-gh <rihannabet1212@gmail.com>
2026-08-05 16:31:19 -0400

    student_scores.py
    A simple Python program that stores student names and scores and checks whether each student passes or fails

--- listreturn.py
+++ listreturn.py
@@ -1,22 +1,32 @@
-list = []
-for i in range(3):
-    names=input("name: ")
-    score=float(input("score: "))
-    list.append({
-        "names" : names ,
-        "score" : score
-                })
-print(list)
-def an(a):
+students = []
 
-    if a>=90:
-     return("yayyyyyyyy")
-    elif a>=50:
-       return("pass")
-    else :
-       return("fail")
-result=an(score)
-print(result)
+for i in range(3):
+    name = input("Name: ")
+    score = float(input("Score: "))
+
+    students.append({
+        "name": name,
+        "score": score
+    })
+
+
+def check_score(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Fail"
+
+
+for student in students:
+    result = check_score(student["score"])
+
+    print("--------------------")
+    print("Name:", student["name"])
+    print("Score:", student["score"])
+    print("Result:", result)
+
 
 
 
