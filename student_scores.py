6d3ffa668188537dd6352050a9f847edbe909d75
Rihanna <rihannabet1212@gmail.com>
2026-08-11 16:03:46 -0400

    Update student_scores.py

--- student_scores.py
+++ student_scores.py
@@ -1,32 +1,37 @@
 students = []
 
-for i in range(3):
-    name = input("Name: ")
-    score = float(input("Score: "))
+for i in range(2):
+    name=input("Name:")
+    score=float(input("Score:"))
+students.append({
 
-    students.append({
-        "name": name,
-        "score": score
-    })
-
-
+    "name": name,
+    "score":score
+})    
 def check_score(score):
-    if score >= 90:
-        return "Excellent"
-    elif score >= 50:
-        return "Pass"
-    else:
-        return "Fail"
+    if score>=85 :
+        return "excellent"
+    elif score>=50 :
+        return "pass"
+    else :
+        return "fail"
 
+for student in students :
 
-for student in students:
-    result = check_score(student["score"])
+    result =check_score(student["score"])
 
-    print("--------------------")
-    print("Name:", student["name"])
+    print("*****************************")
+    print("Name:", student ["name"])
     print("Score:", student["score"])
     print("Result:", result)
 
+highestscore=0
+higheststudent=""
 
-
+for student in students :
+ if highestscore<student["score"] :
+    highestscore=student["score"]
+    higheststudent=student["name"]
+    print("Highestscore:", highestscore)
+    print("Higheststudent:",higheststudent)
 
