0193eb9e4d40a5770428d09360126c79def4c838
Rihanna <rihannabet1212@gmail.com>
2026-08-11 18:00:59 -0400

    Update student_scores.py

--- student_scores.py
+++ student_scores.py
@@ -1,37 +1,49 @@
+
 students = []
 
-for i in range(2):
-    name=input("Name:")
-    score=float(input("Score:"))
-students.append({
+for i in range(3):
+    name = input("Name: ")
+    score= float(input("Score: "))
 
-    "name": name,
-    "score":score
-})    
-def check_score(score):
-    if score>=85 :
-        return "excellent"
-    elif score>=50 :
-        return "pass"
-    else :
-        return "fail"
+    students.append({
+        "name": name,
+        "Score": score
+    })
 
-for student in students :
 
-    result =check_score(student["score"])
+def check_score(x):
+    if x >= 90:
+        return "Excellent"
+    elif x >= 50:
+        return "Pass"
+    else:
+        return "Fail"
+    
 
-    print("*****************************")
-    print("Name:", student ["name"])
-    print("Score:", student["score"])
+for student in students:
+    
+    result = check_score(student["Score"])
+    
+
+    print("--------------------")
+    print("Name:", student["name"])#HAVASET BA FASELE HA BASHE
+    print("Score:", student["Score"])
     print("Result:", result)
 
 highestscore=0
-higheststudent=""
+highestudent=""
+total = 0 
 
-for student in students :
- if highestscore<student["score"] :
-    highestscore=student["score"]
-    higheststudent=student["name"]
-    print("Highestscore:", highestscore)
-    print("Higheststudent:",higheststudent)
+for student in students:
+    if highestscore<student["Score"] :
+     highestscore = student["Score"] 
+     highestudent = student["name"] 
+     total += student["Score"]
+avg = total / len(students)
+print(avg)
+     
+print("Highestscore:", highestscore)
+print("Higheststudent:",highestudent)
+
+
 
